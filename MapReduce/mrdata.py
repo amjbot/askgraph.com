1bfb3f04c6cfe72029af334c4014340dd3604cf8
Ubuntu <ubuntu@ip-10-124-177-74.ec2.internal>
2012-08-17 07:48:18 +0000

    Implement all commands for mrdata manipulations

--- MapReduce/mrdata.py
+++ MapReduce/mrdata.py
@@ -11,7 +11,7 @@ import tornado.database
 db = tornado.database.Connection(host="localhost",user="root",database="root",password="root")
 
 
-if argv[1] == "load" and len(argv)==3:
+if len(argv)==3 and argv[1] == "load":
     dataset = argv[2].rsplit('.',1)[0]
     exists = db.get("SELECT * FROM mr_dataset WHERE dataset_name=%s LIMIT 1", dataset)
     if exists:
@@ -23,29 +23,41 @@ if argv[1] == "load" and len(argv)==3:
         print "Created dataset %s with %d rows" % (dataset,i+1)
 
 
-elif argv[1] == "rm" and len(argv)==3:
+elif len(argv)==3 and argv[1] == "remove":
     count = db.get("SELECT count(*) FROM mr_dataset WHERE dataset_name=%s", argv[2])['count(*)']
     db.execute("DELETE FROM mr_dataset WHERE dataset_name=%s", argv[2])
     print "Removed %s dataset with %d rows" % (argv[2], count or 0)
 
 
-elif argv[1] == "dump" and len(argv)==3:
+elif len(argv)==3 and argv[1] == "show":
     for entry in db.query("SELECT * FROM mr_dataset WHERE dataset_name=%s", argv[2]):
         print entry['dataset_value']
 
 
-elif argv[1] == "list":
+elif len(argv)==2 and argv[1] == "list":
     for entry in db.query("SELECT * FROM mr_dataset GROUP BY dataset_name"):
         print entry["dataset_name"]
 
 
+elif len(argv)==4 and argv[1] == "move":
+    count = db.get("SELECT count(*) FROM mr_dataset WHERE dataset_name=%s", argv[2])['count(*)']
+    db.execute("UPDATE mr_dataset SET dataset_name=%s WHERE dataset_name=%s", argv[3], argv[2])
+    print "Renamed dataset %s with %d rows to %s" % (argv[2], count, argv[3])
+
+
+elif len(argv)==4 and argv[1] == "copy":
+    count = db.get("SELECT count(*) FROM mr_dataset WHERE dataset_name=%s", argv[2])['count(*)']
+    db.execute("INSERT INTO mr_dataset(dataset_name,dataset_value) SELECT %s,dataset_value FROM mr_dataset WHERE dataset_name=%s", argv[3], argv[2])
+    print "Copied dataset %s with %d rows to %s" % (argv[2], count, argv[3])
+
+
 else:
-    print argv
     print "Usage: mrdata.py action [args]"
     print "Actions:"
     print "   list - list available datasets"
     print "   load - load a new dataset from csv"
-    print "   dump - dump a dataset to csv"
+    print "   show - show a dataset in csv format"
+    print "   remove - remove a dataset from the database"
     print "   move - move a dataset to a new location"
     print "   copy - copy a dataset to a new location"
     print "   describe - describe the structure of a dataset"
