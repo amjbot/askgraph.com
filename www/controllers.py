fa7821f66a8516877fd2ce27b1a53292599cb3b3
Ubuntu <ubuntu@ip-10-122-193-149.ec2.internal>
2012-09-18 06:53:24 +0000

    store all headers and remote_ip from request

--- www/controllers.py
+++ www/controllers.py
@@ -1,8 +1,17 @@
 import tornado.web
 import tornado.database
+import json
 
 
 db = tornado.database.Connection(host="localhost",user="root",database="root",password="root")
+
+
+def can_json( o ):
+    try:
+       json.dumps(o)
+       return True
+    except:
+       return False 
 
 
 class index( tornado.web.RequestHandler ):
@@ -11,8 +20,9 @@ class index( tornado.web.RequestHandler ):
     def post( self ):
         contact = self.get_argument("contact","")
         request = self.get_argument("request","")
+        context = json.dumps(dict( (k,v) for (k,v) in vars(self.request).items() if can_json(v) ))
         if not contact or not request:
             self.redirect("/?error=Invalid+request.")
         else:
-            db.execute("INSERT INTO requests(contact,request) VALUES(%s,%s)", contact, request)
+            db.execute("INSERT INTO requests(contact,request,context) VALUES(%s,%s,%s)", contact, request, context)
             self.redirect("/?success=Your+request+is+being+reviewed.")
