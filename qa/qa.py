71cfe86c67b1699e7062d9ad3c68570479003fc9
Ubuntu <ubuntu@ip-10-122-193-149.ec2.internal>
2012-09-20 09:25:50 +0000

    response editing is workig

--- qa/qa.py
+++ qa/qa.py
@@ -6,6 +6,8 @@ import tornado.database
 import json
 import random
 import string
+import tempfile
+import subprocess
 
 
 db = tornado.database.Connection(host="localhost",user="root",database="root",password="root")
@@ -27,8 +29,26 @@ def edit( ticket_id ):
             or {'contact': ''.join(random.choice(string.letters + string.digits) for x in range(16)) })["contact"]
         db.execute("INSERT INTO request_responses(request_id,contact,comment,context) VALUES(%s,%s,%s,%s)",
             request.id, contact, '', json.dumps({}) )
-    t = db.get("SELECT * FROM request_responses WHERE request_id=%s ORDER BY ts desc", ticket_id)
-    print t.request_id, t.id
+    thread = db.query("SELECT * FROM request_responses WHERE request_id=%s ORDER BY ts asc", ticket_id)
+    f = tempfile.NamedTemporaryFile(suffix='_edit_message', mode='w+t', delete=False)
+    message_separator = '\n===============================\n\n'
+    for t in [request]+thread[:-1]:
+        t.context = json.loads(t.context)
+        f.write( 'From ' + t.contact + '\n' )
+        for k in t.context:
+            f.write(k + ': ' + t.context[k] + '\n')
+        f.write( 'Date: ' + t.ts.isoformat() + '\n' )
+        f.write( '\n' )
+        f.write( t.get("request","") or t.get('comment','') )
+        f.write( message_separator )
+    t = db.get("SELECT * FROM request_responses WHERE request_id=%s ORDER BY ts desc LIMIT 1", ticket_id)
+    f.write( t.comment )
+    n = f.name
+    f.close()
+    subprocess.call(['nano', n])
+    with open(n) as f:
+        comment = f.read().split( message_separator )[-1]
+        db.execute("UPDATE request_responses SET comment=%s WHERE id=%s", comment, t.id)
 if len(sys.argv)==3 and sys.argv[1]=='edit': 
     ticket_id = int(sys.argv[2])
     edit( ticket_id = ticket_id )
